3571729d4eff2bcb8d56f481e0f4e5e86655236d
Sleedyak <31696975+Sleedyak@users.noreply.github.com>
2018-02-09 12:14:48 +0100

    probably wrong, but i tried

--- ns.php
+++ ns.php
@@ -1,3 +1,24 @@
 <?php
-  //newsletter stuff will go here
+  $link = @mysql_connect(localhost, "root", "123456");
+  if(!$link){
+   echo "Error connecting to MySQL: " . $mysql_error();
+   exit();
+  }
+  $db = @mysql_selectdb("rcl-ns");
+  if(!$db){
+    echo "Error connecting to the database: " . $mysql_error();
+    exit();
+  }
+  $query = "INSERT INTO ns(email) VALUES($email);";
+  $result = @mysql_query($query);
+  if(!$result){
+    echo "Couldn't add your email. Try again....";
+    exit();
+  }
+  else{
+    @mysql_close($link);
+    echo "<script language='javascript'>";
+    echo "alert('Succesfully saved your email!')";
+    echo "</script>";
+  }
 ?>
