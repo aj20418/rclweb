5316e6bb084c5ef61f47388e22ffe7fdc461f48e
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-02-26 21:41:25 -0500

    i got some more stuff to work

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+# class Member(models.Model):
+#     discord_name = models.CharField(max_length=50)
+
+
+# class Team(models.Model):
+#     team_name = models.CharField(max_length=32)
+#     team_size = models.IntegerField()
+#     team_members = models.ForeignKey([Member for i in range(team_size)], on_delete=models.CASCADE)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 
 
+def load_temp(name):
+    with open('./templates/{}'.format(name)) as f:
+        return f.read()
+
+
 def index(request):
-    with open('./index.html') as f:
-        response = f.read()
-    return HttpResponse(response)
+    return HttpResponse(load_temp('index.html'))
+
+
+def signup(request):
+    return HttpResponse(load_temp('signup.html'))
+
+
+def success(request):
+    return HttpResponse(load_temp('signup.php'))
