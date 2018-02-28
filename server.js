9b690cd2ff9d61cc49fabeb7624077b32072c2c3
the fixer of typos <alexhuang6316.ah@gmail.com>
2018-02-28 17:28:28 -0600

    hmm

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/final'));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/src/index.html');
+  res.sendFile('index.html');
 });
 
 app.get("/test", function (req, res) {
-  res.send(__dirname + '/src/data.html');
+  res.sendFile('stylesheet.css');
 });
 
 const listener = app.listen(8080, function () {
