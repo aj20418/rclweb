43555c5e1feff72526117dd90c58601673a2a749
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-02-28 19:00:45 -0500

    bot idk what ur command is

--- bot/bot.py
+++ bot/bot.py
@@ -2,19 +2,22 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
-filepath = "../data/info.txt"
+filepath = "./data/info.txt"
 with open('config.txt', 'r') as f:
     token = f.read()
-authorized = ['382574338685009920']
+authorized = [
+    281821029490229251,
+    382574338685009920
+]
+
 
 @bot.command()
 async def write(ctx, *, body):
-    if ctx.message.author.id in authorized:
-        with open(filepath, 'r+') as f:
-            f.write(body)
-        await ctx.send(f"Wrote ```{body}``` to {filepath}.")
-    else:
-        await ctx.send("Not authorized.")
+    if not ctx.author.id in authorized:
+        return
+    with open(filepath, 'r+') as f:
+        f.write(body)
+    await ctx.send(f"Wrote ```{body}``` to {filepath}.")
 
 if __name__ == "__main__":
     bot.run(token)
